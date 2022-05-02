730e118ba8e0ddece0ab4005701740e47eb357be
jjdavy225 <jeanjauresdavy@gmail.com>
2022-05-02 20:18:27 +0000

    asset()to->secure_asset()

--- resources/views/demande/index.blade.php
+++ resources/views/demande/index.blade.php
@@ -5,7 +5,7 @@
 @endsection
 
 @section('css1')
-    <link rel="stylesheet" href="{{ asset('css/style1.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/style1.css') }}">
 @endsection
 
 @section('contenu')
